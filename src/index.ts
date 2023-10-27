980e49b94456713e6ecf21ebc8727a48549e5f48
skyclouds2001 <skyclouds2001@163.com>
2023-10-27 14:35:41 +0800

    feat: update base generate

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import fs from 'node:fs'
 import path from 'node:path'
 import prompts from 'prompts'
 import { formatPackageName, formatProjectName } from './format'
-import { isEmptyDir } from './fs'
+import { IGNORES, clearDir, copy, isEmptyDir } from './fs'
 import { isValidPackageName, isValidProjectName } from './validate'
+import url from 'node:url'
 
 enum Framework {
   Vue = 'Vue',
@@ -17,7 +18,7 @@ const cwd = process.cwd()
 void (async function cli() {
   try {
     let dir = DEFAULT_NAME
-    const response = await prompts(
+    const { framework, packageName, projectName } = await prompts(
       [
         {
           type: 'text',
@@ -28,11 +29,6 @@ void (async function cli() {
           onState: (data) => {
             dir = formatProjectName(data.value)
           },
-        },
-        {
-          type: () => (fs.existsSync(dir) && !isEmptyDir(dir) ? 'confirm' : null),
-          name: 'overwrite',
-          message: `${dir === '.' ? 'Current' : 'Target'} dictionary is not empty, remove existing files and continue?`,
         },
         {
           type: 'text',
@@ -65,13 +61,65 @@ void (async function cli() {
       }
     )
 
-    const { framework, packageName, projectName, overwrite } = response
+    // generate the target dictionary path
+    const root = path.join(cwd, formatProjectName(projectName))
 
-    console.log(framework, packageName, projectName, overwrite)
+    // check if the target dictionary is not an empty dictionary
+    // if so, prompt to let user decide whether overwrite the target dictionary
+    // if user decide to overwrite, clear the target dictionary; or if not, exit the process
+    if (fs.existsSync(root) && !isEmptyDir(root)) {
+      const { overwrite } = await prompts([
+        {
+          type: 'confirm',
+          name: 'overwrite',
+          message: `${root === __dirname ? 'Current' : 'Target'} dictionary is not empty, remove existing files and continue?`,
+        },
+      ])
 
-    const root = path.join(cwd, projectName)
+      if (overwrite as boolean) {
+        clearDir(root)
+      } else {
+        throw new Error('Operation cancelled!')
+      }
+    }
 
-    console.log(root)
+    // create target dictionary if it doesn't existed
+    if (!fs.existsSync(root)) {
+      fs.mkdirSync(root)
+    }
+
+    // get the template dictionary name
+    let template: string
+    switch (framework as Framework) {
+      case Framework.Vue:
+        template = 'vite-vue-template-sky'
+        break
+      case Framework.React:
+        template = 'vite-react-template-sky'
+        break
+    }
+    const templateDir = path.resolve(url.fileURLToPath(import.meta.url), '../..', template)
+
+    // copy template project to target
+    for (const file of fs.readdirSync(templateDir)) {
+      if (IGNORES.includes(file) && fs.existsSync(path.resolve(root, file))) {
+        continue
+      }
+
+      if (file === 'node_modules') {
+        continue
+      }
+
+      copy(path.resolve(templateDir, file), path.resolve(root, file))
+    }
+
+    // overwrite package.json name field
+    const pkg = JSON.parse(fs.readFileSync(path.resolve(root, 'package.json'), 'utf-8'))
+    pkg.name = formatPackageName(packageName)
+    fs.writeFileSync(path.resolve(root, 'package.json'), JSON.stringify(pkg, null, 2))
+
+    // print message
+    console.log('Done.')
   } catch (error) {
     if (error instanceof Error) {
       console.log(error.message)
